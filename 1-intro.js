73b393cc5ee47e6120f006ed7217366cbb93708b
hunterallencodes <hunterallen.codes@gmail.com>
2021-11-11 14:11:15 -0600

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,10 @@
+const amount = 10;
+
+if (amount < 10){
+    console.log('small number');
+}
+else {
+    console.log('large number');
+}
+
+console.log(`Hey this is my first app!!!! Hello world!!!`);
